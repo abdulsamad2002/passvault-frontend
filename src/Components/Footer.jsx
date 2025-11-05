ff78963719e420f5a21a17d3b0671fa07dd22bc7
Abdul Samad <abdulsamadsjc2002@gmail.com>
2025-11-06 02:24:24 +0530

    Update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { FaRegCopyright } from "react-icons/fa6";
+import { IoBugSharp } from "react-icons/io5";
+
 
 
 const Footer = () => {
   return (
     <>
       <div className="w-full h-10 fixed bottom-0 bg-[#121212] text-[#E0E0E0] flex justify-center flex-row items-center font-bold gap-4">
-         <FaRegCopyright /> Abdul Samad
+         <FaRegCopyright /> Abdul Samad  <a href="mailto:blackhawkalpha009@gmail.com" className="text-[12px] flex justify-center items-center gap-2">Report <IoBugSharp /></a>
       </div>
     </>
   );
