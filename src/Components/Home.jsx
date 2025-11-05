fb4edf96c1d337c118218d0a3f3abd959efa310b
Abdul Samad <abdulsamadsjc2002@gmail.com>
2025-11-06 02:35:23 +0530

    Update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
     <>
       <div className="mt-20 w-full flex flex-col justify-center items-center transition-all duration-700 gap-10 px-4 text-center overflow-x-hidden">
         <div className="font-bold text-5xl flex justify-center items-center flex-col text-[#E0E0E0] my-4">
-        PassVault
-        <div className="text-[#a9a2a2] text-[15px]">
-          Your own password manager
+          PassVault
+          <div className="text-[#a9a2a2] text-[15px]">
+            Your own password manager
+          </div>
         </div>
-      </div>
 
         <div className="text-white w-full max-w-xl text-sm md:text-base leading-relaxed">
           Pass Vault securely stores your credentials using strong encryption.
@@ -22,17 +22,20 @@ const Home = () => {
 
         <div className="flex flex-col md:flex-row justify-center items-center gap-5 md:gap-10 w-full">
           <button className="border-2 bg-white border-black px-6 py-2 rounded-2xl flex gap-3 items-center hover:bg-[#121212] hover:text-white transition-all duration-300 hover:border-white w-full md:w-auto justify-center">
-            <Link to="/vault">To Vault</Link>
+            <Link to="/vault" className="w-full">
+              To Vault
+            </Link>
           </button>
 
           <button className="border-2 bg-white border-black px-6 py-2 rounded-2xl flex gap-3 items-center hover:bg-[#121212] hover:text-white transition-all duration-300 hover:border-white w-full md:w-auto justify-center">
-            <Link to="/about">Learn More</Link>
+            <Link to="/about" className="w-full">
+              Learn More
+            </Link>
           </button>
         </div>
       </div>
     </>
   );
 };
-
 
 export default Home;
